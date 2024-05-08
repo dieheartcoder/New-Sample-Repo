c736cd7d989d1e82c985ed4987ffe7e1682ce360
dieheartcoder <149042314+dieheartcoder@users.noreply.github.com>
2024-05-08 12:48:49 +0530

    added

--- src/com/pkg/Sum.java
+++ src/com/pkg/Sum.java
@@ -5,7 +5,7 @@ public class Sum {
 	public static void main(String[] args) {
 		
 		System.out.println("Main method...");
-		System.out.println("new line added...");
+		System.out.println("added...");
 				
 	}
 
